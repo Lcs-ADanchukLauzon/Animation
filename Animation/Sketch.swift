558f51c485b17f04fa20c4efa9901ce93c046304
Danchuk-Lauzon, Alexy <adanchuklauzon@lcs-mba3080.lcs.on.ca>
2017-11-06 09:50:53 -0500

    almost there

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -8,7 +8,7 @@ class Sketch : NSObject {
     
     // Position of circle
     var x : Int
-    
+    var height : Int
     // This function runs once
     override init() {
         
@@ -17,18 +17,23 @@ class Sketch : NSObject {
         
         // Set starting position
         x = 0
-        
-    }
+        height = 400
+        canvas.fillColor = Color.black
+        canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 500, height: 500)
     
-    // Runs in a loop, forever, to create the animated effect
+    }
+  
+   // Runs in a loop, forever, to create the animated effect
     func draw() {
         
-        // Change position
+        canvas.drawShapesWithBorders = false
         x += 1
         
-        // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(centreX: x, centreY: Int(10 * sin(Double(20 * x))) + 250, width: 5, height: 5)
-        
+        for i in stride(from: 50, through: 450, by: 25){
+        canvas.fillColor = Color.init(hue: i, saturation: 100, brightness: 100, alpha: 100)
+        canvas.drawEllipse(centreX: x, centreY: Int(50 * sin(Double(0.02631819699 * Double(x)))) + i, width: 5, height: 5)
+        canvas.drawEllipse(centreX: 500 - x, centreY: -Int(50 * sin(Double(0.02631819699 * Double(x)))) + i, width: 5, height: 5)
+        }
     }
     
 }
